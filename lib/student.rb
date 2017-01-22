bf02536a4277f323d181eda93eba24370d2b6344
Abdalkhalek ahmed <eng.abdalkalk@gmail.com>
2017-01-22 19:44:04 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,12 @@ class Student < User
   def initialize
     @knowledge = []
   end
+  def first_name
+    @first_name
+  end
+  def last_name
+    @last_name
+  end
 
   def learn(knowledge)
     @knowledge << knowledge
